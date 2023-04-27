cb5ed1940a15d82c7a4de3da3c83837987b262c0
Eduard-90 <eddard1990@gmail.com>
2023-04-27 20:39:44 +0300

    finish lesson 50

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { AddPostComponent } from './add-post/add-post.component';
+import { UserComponent } from './user/user.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    AddPostComponent
+    UserComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
